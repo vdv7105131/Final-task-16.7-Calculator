dda825b968a8f5dc9cf04b6bad802c231f089496
vdv7105131 <vdv7105131@gmail.com>
2022-04-14 13:55:11 +0300

    Добавьте файлы проекта.

---
+++ Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnSum()
+        {
+            var calculator = new Calculator();
+
+            Assert.True(10 == calculator.Additional(5, 5));
+        }
+
+        [Test]
+        public void SubtractionMustReturnTen()
+        {
+            var calculator = new Calculator();
+                        
+            Assert.True(10 == calculator.Subtraction(15, 5));
+        }
+
+        [Test]
+        public void MultiplicationMustReturnNotNullValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.AreEqual(25 , calculator.Miltiplication(5, 5));
+        }
+
+        [Test]
+        public void DivisionMustReturnNotNullValue()
+        {
+            var calculator = new Calculator();
+
+            Assert.AreEqual(1, calculator.Division(5, 5));
+        }
+
+    }
+}
